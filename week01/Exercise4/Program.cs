6608da0e3820e0c009aec1ced5dfaf7787412427
astegosaurus-jay <jjjester117@gmail.com>
2025-03-05 13:52:40 -0700

    excersice 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int response = -1;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while(response != 0)
+        {
+            
+            Console.Write("Enter a number: ");
+            response = int.Parse(Console.ReadLine());
+            
+            if(response != 0)
+            {
+                numbers.Add(response);
+            }
+            
+        }
+        int sum = 0;
+        foreach(int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"the sum is {sum}");
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"the average is {average}");
+
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"the max number is {max}");
     }
 }
