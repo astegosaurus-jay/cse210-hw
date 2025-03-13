6b2d2db8d5bef90b68349b1f8735fc7b04377f20
astegosaurus-jay <jjjester117@gmail.com>
2025-03-13 14:11:51 -0600

    journal program

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,68 @@
 using System;
+using System.IO.Enumeration;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        DateTime theCurrentTime = DateTime.Now;
+        string dateText = theCurrentTime.ToShortDateString();
+
+
+        PromptGenerator prompt = new PromptGenerator();
+        
+
+
+        
+       
+
+        Journal journal = new Journal();
+
+
+        int response = 0;
+
+        while (response != 5)
+        {
+            Console.WriteLine("Please select one of the following choices: ");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Save to file");
+            Console.WriteLine("4. load a file");
+            Console.WriteLine("5. quit");
+            Console.Write("What would you like to do? ");
+            response = int.Parse(Console.ReadLine());
+
+            if (response == 1)
+            {
+                Entry entry = new Entry();
+                entry._date = dateText;
+                entry._promptText = prompt.GetRandomPrompt();
+                Console.WriteLine(entry._promptText);
+                Console.Write(">");
+                entry._entryText = Console.ReadLine();
+                journal.AddEntry(entry);
+            }
+            else if (response == 2)
+            {
+                journal.DisplayAll();
+            }
+            else if (response == 3)
+            {
+                Console.Write("What is the filename? ");
+                string fileName = Console.ReadLine();
+                journal.SaveToFile(fileName);
+
+            }
+            else if (response == 4)
+            {
+                Console.Write("What is the filename? ");
+                string fileName = Console.ReadLine();
+                journal.LoadFromFile(fileName);
+
+
+            }
+        }
+
     }
 }
--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -1,8 +1,16 @@
 public class PromptGenerator
 {
-    public List<string> _prompts = new List<string>();
+    public List<string> _prompts = ["How was your day", "What did you eat for breakfast", "Who did you interact with", "What did you do for school", "what are your plans for the weekend", "What is your goal today"];
+    
+    
+
+
     public string GetRandomPrompt()
     {
-        return "";
+        Random rnd = new Random();
+        int index = rnd.Next(_prompts.Count);
+        string prompt = _prompts[index];
+
+        return prompt;
     }
 }
