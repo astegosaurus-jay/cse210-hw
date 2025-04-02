aa68ec6768dd0b195886e99aad65af2094db9726
astegosaurus-jay <jjjester117@gmail.com>
2025-04-02 11:31:25 -0600

    homework week 5

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        
+        MathAssignment mathAssignment = new MathAssignment("john", "math", "section 5", "problems 9 - 9000");
+
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("carl", "writing", "The excistence of frogs");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInfo());
     }
 }
