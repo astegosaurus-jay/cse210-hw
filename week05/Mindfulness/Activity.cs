8394051c37d143db469fcc1f75f33ede5da7e183
astegosaurus-jay <jjjester117@gmail.com>
2025-04-02 15:33:25 -0600

    week 5 stub

---
+++ week05/Mindfulness/Activity.cs
@@ -0,0 +1,38 @@
+public class Activity
+{
+    protected string _name;
+
+    protected string _description;
+
+    protected int _duration;
+
+    public Activity(string name, string description, int duration)
+    {
+        _name = name;
+        _description = description;
+        _duration = duration;
+    }
+
+
+    public void DisplayStartingMessage()
+    {
+
+    }
+
+    public void DisplayEndingMessage()
+    {
+
+    }
+
+    public void ShowSpinner(int seconds)
+    {
+
+    }
+
+    public void ShowCountdown(int seconds)
+    {
+        
+    }
+    
+    
+}
---
+++ week05/Mindfulness/BreathingActivity.cs
@@ -0,0 +1,14 @@
+public class BreathingActivity : Activity
+{
+
+    public BreathingActivity(string name, string description, int duration) : base(name, description, duration)
+    {
+
+    }
+
+
+    public void Run()
+    {
+        
+    }
+}
